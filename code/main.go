3e5ae22aacbad513aa69206aeb1069f9203dcd48
serverless-devs <serverless-devs@alibaba-inc.com>
2022-05-25 12:46:25 +0000

    Initial commit

---
+++ code/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"app": "Gin",
+		})
+	})
+	router.Run(":9000")
+}
