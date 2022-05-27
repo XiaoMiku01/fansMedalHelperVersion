a8a77ced314854799d2b5de7296fbaff9aed175b
15619 <1561900932@qq.com>
2022-05-27 20:31:31 +0800

    0.3.0

--- code/main.go
+++ code/main.go
@@ -10,8 +10,8 @@ func main() {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"version":   "0.2.2",
-			"changelog": "\n\n增加异常重试处理\n\n更加详细的报错日志",
+			"version":   "0.3.0",
+			"changelog": "\n\n自动完成每日观看30分钟任务\n\n高度自定义的用户配置（详细看users.yaml文件）\n\n更加详细的微信推送通知（今日亲密度具体获取情况）",
 			"notice":    "",
 		})
 	})
