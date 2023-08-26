029c80a6481fa80e08a6d07fa6ac3d819b0b6596
XiaoMiku01 <loveava0612@outlook.com>
2023-08-26 11:21:33 +0800

    版本更新

--- code/main.go
+++ code/main.go
@@ -10,8 +10,8 @@ func main() {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"version":   "0.3.6",
-			"changelog": "修复：登录失败\n修复：点赞接口失效(鉴权失败提示)\n增加：access_key 过期提示 ，  \n更新方法见文档：doc.loveava.top/ 若更新失败 尝试重新部署 \n",
+			"version":   "0.3.7",
+			"changelog": "修复：点赞失败 ，  \n更新方法见文档：doc.loveava.top/ 若更新失败 尝试重新部署 \n",
 			"notice":    "",
 		})
 	})
